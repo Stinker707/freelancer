d2ff8bee83a1229986ce000902ba1208444206b4
Oleg Ignatev <karambas707@gmail.com>
2025-01-17 14:56:15 +0300

    доделал мобильное меню

---
+++ main.js
@@ -0,0 +1,14 @@
+// const btn = document.querySelector('#close-btn');
+// const menuMobi = document.querySelector('#menu-mobi');
+// btn.onclick = () => {
+//     btn.classList.toggle('header-mobi-close-btn--is-active');
+//     menuMobi.classList.toggle('header-mobi__menu--is-visible');
+// }
+
+const navMenu = document.querySelector('.header-menu');
+const btn = document.querySelector('#close-btn');
+
+btn.onclick = () => {
+    navMenu.classList.toggle('header-menu--is-visible');
+    btn.classList.toggle('header-mobi-close-btn--is-active');
+}
