247cb3ff395d5103cf1f4c8e1de8aa4517110bb3
Олег Игнатьев <164636323+Stinker707@users.noreply.github.com>
2025-03-01 19:55:32 +0300

    чистка main.js

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// const btn = document.querySelector('#close-btn');
-// const menuMobi = document.querySelector('#menu-mobi');
-// btn.onclick = () => {
-//     btn.classList.toggle('header-mobi-close-btn--is-active');
-//     menuMobi.classList.toggle('header-mobi__menu--is-visible');
-// }
-
 const navMenu = document.querySelector('#header-menu');
 const btn = document.querySelector('#close-btn');
 
